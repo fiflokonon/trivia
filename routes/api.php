570a5b8da9ec80d83f2342283721bbd02968f4d5
fiflokonon <fiflokonon@gmail.com>
2023-07-05 19:48:22 +0100

    Is Ok

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});*/
 
 Route::get('/me', [AuthController::class, 'getMe'])->middleware('auth:sanctum');
 Route::post('/login', 'App\Http\Controllers\Api\User\AuthController@login');
